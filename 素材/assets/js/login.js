40a037e581a2f00c6cbbd4f700ad7092cfe78f7d
songjiajun <2801523899@qq.com>
2022-04-24 15:31:09 +0800

    登录与注册

--- 素材/assets/js/login.js
+++ 素材/assets/js/login.js
@@ -1,3 +1,20 @@
 $(function () {
+    $('#reg').on('click', function () {
+        $('.login').hide()
+        $('.reg').show()
+        $('.reg_box').show()
+    })
 
+    $('#login').on('click', function () {
+        $('.login').show()
+        $('.reg').hide()
+    })
+
+    var form = layui.form
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ]
+    })
 })
